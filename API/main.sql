3b32c3b8619cdec054d671e0f07061b4d4cae9e7
goldmeninalmaty <146926829+Ulyk04@users.noreply.github.com>
2024-09-17 20:43:50 +0300

    Update main.sql

--- API/main.sql
+++ API/main.sql
@@ -11,3 +11,14 @@ CREATE TABLE books (
     author VARCHAR(100) NOT NULL,
     available BOOLEAN DEFAULT true NOT NULL
 );
+
+INSERT INTO users(username, password)
+VALUES('username1' , 'pass1')
+('username2' , 'pass2');
+
+INSERT INTO books(title , author , available)
+VALUES('Harry Potter' , 'Liana' , true),
+('My world' , 'Karina' , false);
+
+SELECT * FROM books;
+SELECT * FROM users;
